d85f1ca684e1d598bbba46b0b4f32dc39c481c3c
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-06-10 20:13:47 +0530

    sm8250-common: Add OnePlus Dolby
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 NfcNci RecorderPrebuilt SafetyHubPrebuilt Snap
+LOCAL_OVERRIDES_PACKAGES := Camera2 Snap GalleryGoPrebuilt Gallery2 NfcNci RecorderPrebuilt SafetyHubPrebuilt AudioFX MusicFX SoundAmplifierPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
