dotOS-Devices/android_device_oneplus_sm8150-common

[5.2-Monet] DeviceSettings: Support MonetWannabe on device specific settings *Adapted from https://github.com/dotOS-Devices/device_xiaomi_davinci/commit/2c78474cc767d15f0abaac0408612d8884e9e15f

DeviceSettings/src/com/dot/device/DeviceSettings/MonetPreferenceActivity.java
@@ -1,0 +1,15 @@
+package com.dot.device.DeviceSettings;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class MonetPreferenceActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int monetBackground = getColor(android.R.color.monet_background_device_default);
+        getWindow().getDecorView().setBackgroundColor(monetBackground);
+        getWindow().getDecorView().setBackgroundTintList(ColorStateList.valueOf(monetBackground));
+    }
+}

kunalshah912/device_oneplus_sm8250-common

DeviceExtras/src/org/evolution/device/DeviceExtras/Services/MonetPreferenceActivity.java
@@ -1,0 +1,15 @@
+package org.havoc.device.DeviceSettings;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class MonetPreferenceActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int monetBackground = getColor(android.R.color.monet_background_device_default);
+        getWindow().getDecorView().setBackgroundColor(monetBackground);
+        getWindow().getDecorView().setBackgroundTintList(ColorStateList.valueOf(monetBackground));
+    }
+}
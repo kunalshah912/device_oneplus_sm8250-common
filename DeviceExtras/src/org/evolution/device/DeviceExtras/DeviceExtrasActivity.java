5124fb806ed2d1869b15ed1b9d219072344dad64
Codecity001 <sanganiharish263@gmail.com>
2021-09-21 08:24:52 +0530

    [5.2-Monet] DeviceSettings: Support MonetWannabe on device specific settings *Adapted from https://github.com/dotOS-Devices/device_xiaomi_davinci/commit/2c78474cc767d15f0abaac0408612d8884e9e15f

--- DeviceExtras/src/org/evolution/device/DeviceExtras/DeviceExtrasActivity.java
+++ DeviceExtras/src/org/evolution/device/DeviceExtras/DeviceExtrasActivity.java
@@ -29,8 +29,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
+import com.dot.device.DeviceSettings.MonetPreferenceActivity;
 
-public class DeviceExtrasActivity extends Activity {
+
+public class DeviceExtrasActivity extends MonetPreferenceActivity {
 
     private DeviceExtras mDeviceExtrasFragment;
 
